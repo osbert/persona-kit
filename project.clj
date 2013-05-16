46393371ca5cfb86b238af49d81d8d74aa7333ac
Osbert Feng <osbert@iterinc.com>
2013-05-16 10:27:09 -0700

    Update dependencies.

--- project.clj
+++ project.clj
@@ -3,23 +3,21 @@
   :url "http://github.com/osbert/persona-kit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [hiccup "1.0.2"]
-                 [org.clojure/data.json "0.1.2"]
-                 [clj-http "0.2.6" :exclusions [commons-codec]]
-                 [compojure "1.1.3"]
-                 [com.cemerick/friend "0.1.3" :exclusions [org.clojure/core.incubator org.apache.httpcomponents/httpclient ring/ring-core slingshot]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [hiccup "1.0.3"]
+                 [clj-http "0.7.2" :exclusions [commons-codec]]
+                 [compojure "1.1.5"]
+                 [com.cemerick/friend "0.1.5" :exclusions [org.clojure/core.incubator org.apache.httpcomponents/httpclient ring/ring-core slingshot]]
 
                  ; ClojureScript dependencies
-                 [org.clojure/clojurescript "0.0-1552"]
-                 [shoreleave/shoreleave-remote "0.2.2"]
-                 [domina "1.0.1-SNAPSHOT"]
-                 [org.clojure/google-closure-library "0.0-790"]
-                 [crate "0.2.1"]]
+                 [org.clojure/clojurescript "0.0-1806"]
+                 [shoreleave/shoreleave-remote "0.3.0"]
+                 [domina "1.0.2-SNAPSHOT"]
+                 [crate "0.2.4"]]
   :dev-dependencies [[jline "0.9.94"]
                      [marginalia "0.7.0-SNAPSHOT"]
                      [lein-marginalia "0.7.0-SNAPSHOT"]]
-  :plugins [[lein-cljsbuild "0.2.9"]]
+  :plugins [[lein-cljsbuild "0.3.2" :exclusions [org.clojure/clojure]]]
   :min-lein-version "2.0.0"
   :hooks [leiningen.cljsbuild]
   :cljsbuild {:crossovers [persona-kit.uris]
