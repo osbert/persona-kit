b4ed5b8113d53a7cae4e6e085fe7598a6b095f83
Osbert Feng <osbert@iterinc.com>
2013-05-16 13:20:22 -0700

    Refactor into smaller methods and wrap with wrap-anti-forgery.

--- src/persona_kit/friend.clj
+++ src/persona_kit/friend.clj
@@ -5,7 +5,8 @@
             [compojure.core :as c]
             [ring.util.response :as r])
   (:use [persona-kit.core :only [valid? verify-assertion]]
-        [persona-kit.uris :only [*login-uri* *logout-uri*]]))
+        [persona-kit.uris :only [*login-uri* *logout-uri*]]
+        [ring.middleware.anti-forgery :only [wrap-anti-forgery]]))
 
 (defn credential-fn [{:keys [email] :as credential-map}]
   (when (valid? credential-map)
@@ -21,13 +22,23 @@
         credential-fn
         (workflows/make-auth {::friend/redirect-on-auth? false ::friend/workflow :mozilla-persona}))))
 
+(def login-handler
+  (c/make-route :post *login-uri*
+                (fn [request]
+                  (-> (r/response (pr-str (friend/identity request)))
+                      (r/content-type "application/clojure; charset=utf-8")))))
+
+(def logout-handler
+  (friend/logout
+   (c/POST *logout-uri* []
+           (r/response "OK"))))
+
+(def wrap-persona-friend*
+  "Insecure version without wrap-anti-forgery."
+  (c/routes login-handler logout-handler))
+
 (defn wrap-persona-friend
   "Ring middleware that handles login and logout requests."
   [handler]
-  (c/routes
-   (c/make-route :post *login-uri*
-                 (fn [request]
-                   (-> (r/response (pr-str (friend/identity request)))
-                       (r/content-type "application/clojure; charset=utf-8"))))
-   (friend/logout (c/POST *logout-uri* [] (r/response "OK")))
-   #(handler %)))
+  (c/routes (wrap-anti-forgery wrap-persona-friend*)
+            #(handler %)))
